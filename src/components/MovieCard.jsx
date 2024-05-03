3281e09676e03a3ec38ec05ae0fe8689e5dcbbf4
sakthivaishnavi <sakthivaishnavi12@gmail.com>
2024-05-03 23:53:09 +0530

    icon fix

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types"
 const MovieCard = (props) =>{
     return(
-        <div className="card" style={{width: "18rem"}}>
-  <img src={props.image} className="card-img-top" alt={props.alt}/>
+        <div className="card" style={{width: "18rem" , height: "20px"}}>
+  <img src={props.image} className="card-img-top h-auto w-auto" alt={props.alt}/>
   <div className="card-body">
     <h5 className="card-title">{props.title}</h5>
     <p className="card-text">{props.desc}</p>
